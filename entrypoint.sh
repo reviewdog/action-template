80bd5ef9cce0199e2a3c762fb234793594f2e235
haya14busa <haya14busa@gmail.com>
2022-04-14 00:07:17 +0900

    fix the git issue
    ref: https://github.com/reviewdog/reviewdog/issues/1158

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 if [ -n "${GITHUB_WORKSPACE}" ] ; then
   cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
+  git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
 fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
